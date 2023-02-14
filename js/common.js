f00c0e3bfff265f247bc383b17ba8b41341e8901
sewon <kimsewon0076@gmail.com>
2023-02-14 09:49:24 +0900

    dothome change

--- js/common.js
+++ js/common.js
@@ -1,15 +1,14 @@
-let cursor = document.getElementsByClassName('cursor');
-let posX =0;
+let cursor = document.getElementsByClassName("cursor");
+let posX = 0;
 let posY = 0;
-document.addEventListener('DOMContentLoaded',()=>{
-    document.body.onmousemove = function(e){
-        posX = e.clientX + 'px';
-        posY = e.clientY +"px";
-        cursor[0].style.display="block";
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.onmousemove = function (e) {
+        posX = e.clientX + 4 + "px";
+        posY = e.clientY + 4 + "px";
+        cursor[0].style.display = "block";
         cursor[0].style.left = posX;
-        cursor[0].style.top = posY;  
+        cursor[0].style.top = posY;
         console.log(posX);
         console.log(posY);
-
-    }
-})
+    };
+});
